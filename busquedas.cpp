496ca4b4c54a77e3f0382d01e5f03abda0198b1a
Jorge <jorger@tec.mx>
2020-08-27 11:34:05 -0500

    agregadas busquedas binarias y ordenada2 iterativa

--- busquedas.cpp
+++ busquedas.cpp
@@ -57,7 +57,8 @@ int busquedaBinariaAux(vector<int> a, int buscado, int inicio, int fin){
 	}
 }
 
-int busquedaBinaria(vector<int> a, int buscado){
+
+int busquedaBinariaRecursiva(vector<int> a, int buscado){
 	if(buscado>a[a.size()-1]|| buscado<a[0]){
 		return -1;
 	}else{
@@ -65,42 +66,85 @@ int busquedaBinaria(vector<int> a, int buscado){
 	}
 }
 
-int busquedaOrdenada2(vector<int> a, int buscado){
+int busquedaOrdenada2Recursiva(vector<int> a, int buscado){
 	int paso=10000;
 	return busquedaOrdenada2Aux(a, buscado, 0, paso, paso);
 }
 
+
+int busquedaOrdenada2(vector<int> a, int buscado){
+	int paso=2;
+	int inicio=0;
+	int fin;
+	while(inicio<a.size()){
+		fin=inicio+paso;
+		if(fin>a.size()){
+			fin=a.size();
+		}
+		if(a[fin-1]>=buscado){
+			for(int i=inicio; i<fin; i++){
+				if(a[i]==buscado){
+					return i;
+				}
+			}
+			return -1;
+		}
+		inicio=fin;
+	}
+	return -1;
+}
+
+int busquedaBinaria(vector<int> a, int buscado){
+	int inicio=0;
+	int fin=a.size()-1;
+	while(fin>=inicio){
+		int medio=(inicio+fin)/2;
+		if(a[medio]==buscado){
+			return medio;
+		}else if(buscado>a[medio]){
+			inicio=medio+1;
+		}else{
+			fin=medio-1;
+		}
+	}
+	return -1;
+}
 
 int main(){
 	vector<int> a;
 	std::default_random_engine generator;
 	std::uniform_int_distribution<int> distribution(1,10000000);
 	for(int i=0; i<1000000; i++){
+		//a.push_back(i);
 		a.push_back(distribution(generator));
 	}
-	int buscado=a[0];
-	sort(a.begin(), a.end());
-	int x;
 	
+	sort(a.begin(), a.end());
+	int buscado=a[999999];
+	int x;
+	//Busqueda secuencial
+	cout<<"Secuencial ";
 	auto inicio=high_resolution_clock::now();
 	x=busquedaSecuencial(a,buscado);
 	auto fin=high_resolution_clock::now();
 	auto tiempo=duration_cast<microseconds>(fin - inicio).count();
 	cout<<"posicion: "<<x<<", tiempo: "<<tiempo<<endl;
 	//Busqueda Ordenada 1
-	cout<<"Ordenada 1"<<endl;
+	cout<<"Ordenada 1 ";
 	inicio=high_resolution_clock::now();
 	x=busquedaOrdenada1(a,buscado);
 	fin=high_resolution_clock::now();
 	tiempo=duration_cast<microseconds>(fin - inicio).count();
 	cout<<"posicion: "<<x<<", tiempo: "<<tiempo<<endl;
-	/*cout<<"Ordenada 2"<<endl;
+	//Busqueda Ordenada 2
+	cout<<"Ordenada 2 ";
 	inicio=high_resolution_clock::now();
 	x=busquedaOrdenada2(a,buscado);
 	fin=high_resolution_clock::now();
 	tiempo=duration_cast<microseconds>(fin - inicio).count();
-	cout<<"posicion: "<<x<<", tiempo: "<<tiempo<<endl;*/
-	cout<<"Binario"<<endl;
+	cout<<"posicion: "<<x<<", tiempo: "<<tiempo<<endl;
+	//Busqueda binaria
+	cout<<"Binaria ";
 	inicio=high_resolution_clock::now();
 	x=busquedaBinaria(a,buscado);
 	fin=high_resolution_clock::now();
